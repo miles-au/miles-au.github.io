36803b64f82cef6b2db4c6a5b23ed89a4a8f06ba
Miles Au <miles.d.au@gmail.com>
2020-08-02 12:41:03 -0400

    body max width, and rounded corners on images and callouts

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,6 +5,15 @@ import Projects from '../Projects/Projects';
 function Body() {
   return (
     <div className="body">
+      <p>Mobile + Web Dev with interest in AR</p>
+      <div className="callouts">
+        <a className="callout" href="https://www.youtube.com/playlist?list=PLUQm-VQnY5s3NSJ9fbZ7EEKgkBDZSwEZO" target="_blank">
+          <img src="https://github.com/miles-au/miles-au.github.io/blob/develop/public/images/youtube.png?raw=true" />Checkout my App Showcase Playlist on YouTube
+        </a>
+        <a className="callout" href="https://github.com/miles-au/miles-au.github.io/blob/develop/public/assets/Miles%20Au%20-%20Resume.pdf" target="_blank">
+          <img src="https://github.com/miles-au/miles-au.github.io/blob/develop/public/images/file.png?raw=true" />Click here to checkout my resume
+        </a>
+      </div>
       <Projects />
     </div>
   );
